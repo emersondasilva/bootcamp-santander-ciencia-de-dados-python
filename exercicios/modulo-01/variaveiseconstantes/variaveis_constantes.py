30eca98f91e4adf02650c5845bf6673086d12e32
emersondasilva <emersondssouza04@gmail.com>
2026-01-01 22:50:45 -0300

    Adicionei os exercícios do módulo 01

---
+++ exercicios/modulo-01/variaveiseconstantes/variaveis_constantes.py
@@ -0,0 +1,10 @@
+nome = "Guilherme"
+idade = 28
+
+nome, idade = "Giovanna", 27
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILLIAN_STATES = ["SP", "RJ", "SC", "RS"]
+print(BRAZILLIAN_STATES)
